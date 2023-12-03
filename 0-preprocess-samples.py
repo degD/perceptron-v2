a055a2509f740258384c6e3f879feb9bce4c4b63
degD <108623049+degD@users.noreply.github.com>
2023-12-03 17:14:24 +0300

    Script to decode samples

---
+++ 0-preprocess-samples.py
@@ -0,0 +1,19 @@
+
+from unidecode import unidecode
+import string
+
+sample_files = ['allsamples.txt', 'trainingsamples.txt', 'testingsamples.txt']
+for filename in sample_files:
+    
+    # Process file. Remove punctuation, turn to lower case, and turn characters to ASCII
+    with open(filename) as sf:
+        
+        file_text = ''.join(sf.readlines())
+        file_text = file_text.lower()
+        file_text = unidecode(file_text)
+        file_text = ''.join([ch for ch in file_text if ch not in string.punctuation])
+    
+    # Write text back to file
+    with open(filename, 'w') as sf:
+        
+        sf.write(file_text)
