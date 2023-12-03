02b41636c017391ab39adf0a83fd0c244ab4e750
degD <108623049+degD@users.noreply.github.com>
2023-12-03 12:52:48 +0300

    Add test.c

--- 4-test.c
+++ 4-test.c
@@ -5,24 +5,78 @@
 
 
 int getD();
+int getN();
+double multiplication_of_wx(int *singleHotVector, double *parameters);
 
 
 int main()
 {
-    FILE *testSamplesPtr, *modelPtr, *testTruesPtr;
-    double *parameters;
+    int D, N, **hotVectors, *y_true, number_of_successful = 0;
+    FILE *hvPtr, *modelPtr, *testTruesPtr;
+    double *parameters, y_hat;
+    char ch;
 
-    testSamplesPtr = fopen("testingsamples.txt", "r");
+    D = getD();
+    N = getN();
+    // printf("%d, %d\n", D, N);
+
+    hvPtr = fopen("testinghv.txt", "r");
     modelPtr = fopen("model.txt", "r");
     testTruesPtr = fopen("testingtrues.txt", "r");
 
-    if (testSamplesPtr == NULL || modelPtr == NULL || testTruesPtr == NULL) 
+    if (hvPtr == NULL || modelPtr == NULL || testTruesPtr == NULL) 
     {
         puts("!! Unable to open files");
         return -1;
     }
 
+    // Read parameters
+    parameters = calloc(D, sizeof(double));
+    for (int i = 0; i < D; i++) {
+        fscanf(modelPtr, "%lf", &parameters[i]);
+        // printf("%3d: % lf\n", i, parameters[i]);
+    }
+    fclose(modelPtr);
 
+    // Read hot vectors
+    hotVectors = calloc(N, sizeof(int *));
+    for (int i = 0; i < N; i++) 
+    {
+        hotVectors[i] = calloc(D, sizeof(int));
+        for (int j = 0; j < D; j++) fscanf(hvPtr, "%d", &hotVectors[i][j]);
+    }
+    // for (int i = 0; i < N; i++) {
+    //     for (int j = 0; j < D; j++) printf("%d ", hotVectors[i][j]);
+    //     puts("");
+    // }
+    fclose(hvPtr);
+
+    // Read true "y" values
+    y_true = calloc(N, sizeof(int));
+    for (int i = 0; i < N; i++) 
+    {
+        ch = fgetc(testTruesPtr);
+        fgetc(testTruesPtr);
+        
+        if (ch == '+') y_true[i] = +1;
+        else y_true[i] = -1;
+    }
+    fclose(testTruesPtr);
+
+    // Test the model
+    for (int i = 0; i < N; i++) 
+    {
+        y_hat = tanh(multiplication_of_wx(hotVectors[i], parameters));
+        printf("% lf, %d\n", y_hat, y_true[i]);
+        y_hat = y_hat > 0 ? 1 : -1;
+        if (y_hat == y_true[i]) number_of_successful++;
+    }
+    printf("Number of successful predictions: %d/%d\n", number_of_successful, N);
+    printf("Prediction accuracy: %.2lf%%\n", 100 * ((double)number_of_successful / N));
+
+    for (int i = 0; i < N; i++) free(hotVectors[i]);
+    free(hotVectors);
+    free(y_true);
 
     return 0;
 }
@@ -46,23 +100,33 @@ int getD()
 }
 
 
-// Get number of test samples
+// Get number of test samples (hot vectors)
 int getN()
 {
-    FILE *tsPtr = fopen("testingsamples.txt", "r");
+    FILE *hvPtr = fopen("testinghv.txt", "r");
     int N = 0;
     char ch;
 
-    if (tsPtr == NULL) {
-        puts("!! Unable to read 'testingsamples.txt'");
+    if (hvPtr == NULL) {
+        puts("!! Unable to read 'testinghv.txt'");
         return -1;
     }
     
     do {
-        ch = fgetc(tsPtr);
+        ch = fgetc(hvPtr);
         if (ch == '\n') N++;
     }
+    while(ch != EOF);
 
-    fclose(tsPtr);
+    fclose(hvPtr);
     return N;
+}
+
+
+double multiplication_of_wx(int *singleHotVector, double *parameters)
+{
+    int D = getD();
+    double m = 0;
+    for (int i = 0; i < D; i++) m += singleHotVector[i] * parameters[i];
+    return m;
 }
