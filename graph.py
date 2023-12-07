c1fa7291d9cd7ea128a43f59c88565233c9eb995
degD <108623049+degD@users.noreply.github.com>
2023-12-07 09:49:25 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -3,9 +3,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(3, 2, layout="constrained")
+fig, axs = plt.subplots(3, 2, layout="constrained", sharey=True)
 
-# Epoch vs loss
 for i in range(3):
     
     os.system(f"gcc 3-train.c -lm -o ./3.out; echo {i} | ./3.out")
@@ -15,8 +14,10 @@ for i in range(3):
         # Acquiring data
         data = tlog.readlines()
         epoch_range = len(data) - 1
-        loss_data = np.array([float(line.split()[1]) for line in data[:-1]])        
+        time_range = data[-1].split()
+        loss_data = np.array([float(line.split()[1]) for line in data[:-1]])
     
+    # Epoch vs loss
     axs[i][0].plot(np.arange(epoch_range), loss_data)
 
     if i == 0: 
@@ -32,21 +33,7 @@ for i in range(3):
         axs[i][0].set_ylabel("loss")
         axs[i][0].set_xlabel("epoch")
     
-    plt.xlabel("time")
-    plt.ylabel("epoch")
-
-# Time vs loss
-for i in range(3):
-    
-    os.system(f"gcc 3-train.c -lm -o ./3.out; echo {i} | ./3.out")
-    
-    with open("training.log") as tlog:
-        
-        # Acquiring data
-        data = tlog.readlines()
-        time_range = data[-1].split()
-        loss_data = np.array([float(line.split()[1]) for line in data[:-1]])
-    
+    # Time vs loss
     axs[i][1].plot(np.linspace(0, float(time_range[0]), len(loss_data)), loss_data)
 
     if i == 0: 
@@ -61,5 +48,5 @@ for i in range(3):
         axs[i][1].set_title("ADAM - time (ms) vs loss")
         axs[i][1].set_ylabel("loss")
         axs[i][1].set_xlabel("time")
-    
+
 plt.show()
